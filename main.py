9a157467aac3962f4509918ab585ab25469418b5
Alex <ynxela@gmail.com>
2019-05-18 11:35:59 +0300

    Исправление ошибок, увеличение скорости работы
    Теперь программа не вылетает с ошибками
    Также Увеличина скорость до 60 раз

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ post_pattern_len = 5
 first_index = 0
 
 
-# Проверяет, подходит ли паттерн. Критерий - символы в паттерне должны быть строго из множества mask
+# Проверяет, подходит ли паттерн
 def pattern_is_valid(pattern):
-    if len(pattern) > 1 and '\n' in pattern:
+    if '\n' in pattern and len(pattern) > 1:
         return False
     for i in pattern:
         if i not in mask:
@@ -27,29 +27,21 @@ def pattern_is_valid(pattern):
 def add_pattern(p_len, last_index, line, root):
     for index in range(first_index, last_index - p_len + 2):
         pattern = line[index: index + p_len]
-        # Добавляет паттерн в массив, если его там нет, проверяя, соответствует ли критериям
-        if pattern not in root and '\n' not in pattern and pattern_is_valid(pattern):
+        if pattern not in root and '\n' not in pattern and pattern_is_valid(pattern):  # Добавляет паттерн в массив
             root[pattern] = {}
-
-
-# Функция заполняет словарь символами, идущими следом за паттерном
-def add_pattern_post_patterns(pattern, value, line, last_index):
-    p_len = len(pattern)
-    for index in range(first_index, last_index - p_len + 2):
-        if line[index:index + p_len] == pattern:  # Соответствует ли срез паттерну? Если нет - пропускаем.
-            # Находим максимальную длину пост-паттерна
-            max_post_pattern_len = last_index - index - p_len + 1
+            root[pattern] = {}
+        if pattern in root:
+            max_post_pattern_len = last_index - index - p_len + 1  # Максимальная длина пост-паттерна
             num_of_post_patterns = post_pattern_len
             if max_post_pattern_len < post_pattern_len:
                 num_of_post_patterns = max_post_pattern_len
-            # Находим все пост-паттерны
-            for i in range(1, num_of_post_patterns + 1):
+            for i in range(1, num_of_post_patterns + 1):  # Находим все пост-паттерны
                 post_pattern = line[index + p_len: index + p_len + i]
                 if pattern_is_valid(post_pattern):
-                    if post_pattern in value['post_pattern']:
-                        value['post_pattern'][post_pattern] += 1
+                    if post_pattern in root[pattern]:
+                        root[pattern][post_pattern] += 1
                     else:
-                        value['post_pattern'][post_pattern] = 1
+                        root[pattern][post_pattern] = 1
 
 
 def safe_data(data, file):
@@ -58,47 +50,9 @@ def safe_data(data, file):
 
 
 def get_post_pattern(base_pattern, data):
-    post_patterns = data[base_pattern]['post_pattern']
+    post_patterns = data[base_pattern]
     next_s = random.choices(list(post_patterns.keys()), list(post_patterns.values()))[0]
     return next_s
-
-
-def write_text(count_symbols):
-    with open(data_base, "r") as write_file:
-        root_structure = json.loads(write_file.read())
-    text = ''
-    base_pattern = random.choice(mask)
-    for i in range(count_symbols):
-        if len(text) > 6:
-            base_pattern = text[-5:]
-        try:
-            letter = get_post_pattern(base_pattern, root_structure)
-        except:
-            try:
-                base_pattern = text[-4:]
-                letter = get_post_pattern(base_pattern, root_structure)
-            except:
-                try:
-                    base_pattern = text[-3:]
-                    letter = get_post_pattern(base_pattern, root_structure)
-                except:
-                    try:
-                        base_pattern = text[-2:]
-                        letter = get_post_pattern(base_pattern, root_structure)
-                    except:
-                        try:
-                            base_pattern = text[-1:]
-                            letter = get_post_pattern(base_pattern, root_structure)
-                        except:
-                            base_pattern = random.choice(mask)
-                            letter = get_post_pattern(base_pattern, root_structure)
-        text += letter
-        base_pattern = letter
-
-    with open(output_text, 'ta', encoding='utf-8') as out_text:
-        out_text.write(
-            '{0} | '.format(time.ctime()) + 'длина паттерна - {0} | '.format(
-                pattern_len) + 'длина пост-паттерна - {0}'.format(post_pattern_len) + '\n' + text + '\n\n')
 
 
 def fill_db(flag):
@@ -106,41 +60,33 @@ def fill_db(flag):
         root_structure = json.loads(write_file.read())
         if len(root_structure) == 0 or flag == 1:
             root_structure = {}
-
-            # Количество каждого символа и пары в тексте
-            with open(file_name, 'r') as text:
+            with open(file_name, 'r') as text:  # Количество каждого символа и пары в тексте
                 for line in text:  # Для каждой линии в тексте
                     line = line.lower()
                     last_index = len(line) - 1  # Последний индекс в строке
-                    # Добавляем паттерны всех длин
-                    for p_len in range(1, pattern_len + 1):
+                    for p_len in range(1, pattern_len + 1):  # Добавляем паттерны всех длин
                         add_pattern(p_len, last_index, line, root_structure)
-
-            # Количество встреч каждого символа после искомого символа (key):
-            for key, value in root_structure.items():
-                value['post_pattern'] = {}  # Добавили символ для анализа
-                # Здесь начинаем прогонять символы и забивать данные в словарь, который создан под данную букву
-                with open(file_name, 'r') as text:
-                    for line in text:
-                        line = line.lower()
-                        last_index = len(line) - 1
-                        if key in line:
-                            add_pattern_post_patterns(key, value, line, last_index)
-
-            # # Если пара или тройка ни разу не встретилась в тексте, то удалить
-            # keys_to_delete = []
-            # for key, value in root_structure.items():
-            #     if len(value['post_pattern']) == 0:
-            #         keys_to_delete.append(key)
-            # for key in keys_to_delete:
-            #     del root_structure[key]
-
-            # del root_structure[' ']['post_pattern'][' ']
-            # for i in range(2, pattern_len + 1):
-            #     keys_to_delete = '{}'.format(' ' * i)
-            #     del root_structure[keys_to_delete]
-
             safe_data(root_structure, data_base)
+
+
+def write_text(count_symbols):
+    text = ''
+    with open(data_base, "r") as write_file:
+        root_structure = json.loads(write_file.read())
+    for i in range(count_symbols):
+        post_pattern = None
+        for pat in range(pattern_len, -1, -1):
+            base_pattern = text[-pat:]
+            if base_pattern in root_structure:
+                post_pattern = get_post_pattern(base_pattern, root_structure)
+                break
+            if pat == 0:
+                post_pattern = random.choice(mask)
+        text += post_pattern
+    with open(output_text, 'ta', encoding='utf-8') as out_text:
+        out_text.write(
+            '{0} | '.format(time.ctime()) + 'длина паттерна - {0} | '.format(
+                pattern_len) + 'длина пост-паттерна - {0}'.format(post_pattern_len) + '\n' + text + '\n\n')
 
 
 def check_existing_db():
